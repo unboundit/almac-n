4e2462d769e0a38d3dcf565bcbed44d693d50fcf
Juan Salvador Villalobos Grijalva <Juan Salvador Villalobos Grijalva>
2016-11-19 15:51:44 -0600

    datos de la conexion

--- config.php
+++ config.php
@@ -13,10 +13,10 @@
   //mysql.hostinger.mx
 
 
-	define( "DB_DSN", "mysql:127.0.0.1;dbname=almacen" );
-	define( "DB_USERNAME", "root" );
-	define( "DB_PASSWORD", "" );
+	define( "DB_DSN", "mysql:127.0.0.1;dbname=u300697298_almcn" );
+	define( "DB_USERNAME", "u300697298_pollo" );
+	define( "DB_PASSWORD", "pass_u300697298.,m" );
 	define( "DB_HOST", "localhost");
-	define( "DB_NAME", "almacen");
+	define( "DB_NAME", "u300697298_almcn");
 
 ?>
